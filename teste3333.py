015df6810df8ce2449e588f265a76219c014225f
Daniel Delattre <daniieeldelattre@gmail.com>
2019-10-16 03:26:38 -0300

    funcionamento parcialmente adicionado

--- teste3333.py
+++ teste3333.py
@@ -76,3 +76,64 @@ while quer_jogar == True:
 
     cartas_croupier.append(baralho.pop())
     cartas_croupier.append(baralho.pop())
+
+    lista_calcula_mao = []
+    #cartas
+
+    print('Com quantos baralhos vocês querem jogar?')
+    snum_b = input('Digite entre 1 e 5: ')
+    while snum_b not in ['1', '2', '3', '4', '5']:
+        snum_b = input('Valor inválido!\nDigite um valor entre 1 e 5: ')
+    num_b = int(snum_b)
+    baralho = num_b*lista_b
+
+    limpa()
+
+
+
+    for nome in dic_players_cartas:
+        #lista_cartas = [dic_players_cartas[nome]]
+        cont = 1 
+        cont_c = 1
+        while cont_c != 0:
+            printa_carteira()
+            pula_linha
+            print(f'{nome},\nQuanto você quer apostar?')
+            sv_aposta = input('... ')
+            v_aposta = int(sv_aposta)
+            dic_players_carteira[nome] -= v_aposta
+            printa_carteira()
+            cont_c = 0
+            
+        while cont != 0:
+            if dic_players_carteira[nome] >= 0:
+                printa_cartas()
+                print(f'As cartas do croupier [{cartas_croupier[0]}][?]')            
+                print('\n[1] para mais uma carta.\n[2] para parar.')
+                
+                escolha = input('Escolha: ')
+                pula_linha()
+                if escolha == '1':
+                    #lista_cartas.append(baralho.pop())
+                    dic_players_cartas[nome].append(baralho.pop())
+                if escolha == '2':
+                    print('Fim do turno!')
+                    pula_linha()
+                    cont = 0
+                
+                if calcula_mao(dic_players_cartas[nome]) == 21:
+                    print(f'{nome},\nSuas cartas são: {dic_players_cartas[nome]}, Total = [{calcula_mao(dic_players_cartas[nome])}]')
+                    pula_linha()
+                    print('Fim do turno!')
+                    pula_linha()
+                    cont = 0
+                elif calcula_mao(dic_players_cartas[nome]) > 21:
+                    print(f'{nome},\nSuas cartas são: {dic_players_cartas[nome]}, Total = [{calcula_mao(dic_players_cartas[nome])}]')
+                    pula_linha()
+                    print('Fim do turno!')
+                    pula_linha()
+                    cont = 0
+            else: 
+                printa_carteira()
+                break
+
